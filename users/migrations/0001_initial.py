62c27219cbe9b08ab505ab6e7ecd05999cfbff09
tugruldonmez <111467883+tugruldonmez@users.noreply.github.com>
2023-03-17 20:35:55 +0300

    17.03.2023--19:49
    user has no attribute -debugged

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Profile',
+            name='Profile',#bu niye büyük p
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('image', models.ImageField(default='default.jpg', upload_to='profil_foto')),
--- users/models.py
+++ users/models.py
@@ -4,18 +4,18 @@ from PIL import Image
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self, **kwargs):
         super().save()
 
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:#resim boyutları 300x300 den büyükse yeniden yapılandıran if
+        if img.height > 300 or img.width > 300:  # resim boyutları 300x300 den büyükse yeniden yapılandıran if
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
