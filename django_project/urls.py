1be24e1bf6e0317d7c595f4306cd93ac28fd5ca3
tugruldonmez <111467883+tugruldonmez@users.noreply.github.com>
2023-03-20 19:58:31 +0300

    20.03.2023--19:58
    -global settings has changed(for this project)
    -password reset
    -EMAIL_HOST_USER = 'tugruldonmez23@gmail.com'***

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,12 +26,12 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', 
+
+    path('password-reset/',
         auth_views.PasswordResetView.as_view(
             template_name='users/password_reset.html'
             ), 
         name='password_reset'),
-
     path('password-reset/done/', 
         auth_views.PasswordResetDoneView.as_view(
             template_name='users/password_reset_done.html'
@@ -43,6 +43,11 @@ urlpatterns = [
             ), 
         name='password_reset_confirm'),
     path('', include('blog.urls')),
+    path('password-reset/complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+            ),
+        name='password_reset_complete'),
 ]
 
 
